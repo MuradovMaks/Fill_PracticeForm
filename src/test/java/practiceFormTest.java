f38b724059b3eb110edbaccd1bc93bd8352e0159
Maxim Muradov <koreantech620@mail.ru>
2024-04-19 17:11:51 +0700

    Refactoring Test Project

--- src/test/java/practiceFormTest.java
+++ src/test/java/practiceFormTest.java
@@ -1,64 +1,40 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.commands.SelectOptionByTextOrIndex;
-import com.codeborne.selenide.selector.ByText;
-import netscape.javascript.JSObject;
-import org.apache.commons.exec.OS;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptException;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.devtools.idealized.Javascript;
-
-import javax.tools.StandardJavaFileManager;
-import java.io.File;
 
 import static com.codeborne.selenide.Selenide.*;
 
-public class PracticeForm
-{
+public class practiceFormTest {
     @BeforeEach
-    void SetUp()
-    {
+    void setUp() {
         Configuration.pageLoadStrategy = "eager";
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
+
 
     }
 
 
     @Test
-    void PracticeForm()
-    {
-
-
-
-
+    void practiceForm() {
         open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
         $("[id=firstName]").setValue("Maxim");
         $("[id=lastName]").setValue("Muradov");
         $("[id=userEmail]").setValue("koreantech620@mail.ru").pressEnter();
         $("label[class=custom-control-label]").click();
-
         $("[id=userNumber]").setValue("79836025158");
         $("[id=dateOfBirthInput]").click();
-        //$(".react-datepicker__year-select").click();
         $("[class=react-datepicker__year-select]>[value='2000']").click();
-        //$("[class=react-datepicker__month-select]").click();
         $("[class=react-datepicker__month-select]>[value='3']").click();
-        //$("[class=react-datepicker__day react-datepicker__day--025]").click();
         $(".react-datepicker__month .react-datepicker__day--024").click();
-
         $("[id=subjectsContainer]").click();
         $("[id=subjectsInput]").setValue("English").pressEnter();
         $("[id=subjectsInput]").setValue("Maths").pressEnter();
         $("#hobbiesWrapper > div.col-md-9.col-sm-12 > div:nth-child(1) > label").click();
-        //$("[id=uploadPicture]").doubleClick();
-        File file = new File("C:\\Users\\Murad\\IdeaProjects\\Fill_PracticeForm\\src\\test\\resources\\D.jpg");
-        $("[id=uploadPicture]").uploadFile(file);
+        $("[id=uploadPicture]").uploadFromClasspath("D.jpg");
         $("[id=currentAddress]").click();
         $("[id=currentAddress]").setValue("Lenina 25");
         $("[id=react-select-3-input]").setValue("NCR").pressEnter();
