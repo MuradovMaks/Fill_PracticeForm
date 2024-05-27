d95fa54ef719eb63f3bebe256ed4d45aa95967f7
Maxim Muradov <koreantech620@mail.ru>
2024-05-28 02:16:50 +0700

    Add managing of parametries df

--- src/test/java/properties/tests/PropertiesSystem.java
+++ src/test/java/properties/tests/PropertiesSystem.java
@@ -12,6 +12,7 @@ public class PropertiesSystem
     @Tag("PracticeForm")
     void BrowserPropertie()
     {
+        System.setProperty("Wdhost","https://user1:1234@selenoid.autotests.cloud/wd/hub");
         Configuration.remote = System.getProperty("Wdhost");
         browser = System.getProperty("Browser");
         Configuration.browserVersion = System.getProperty("BrowserVersion");
