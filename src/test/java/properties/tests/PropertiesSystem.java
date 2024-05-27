32e0ab910bb2c302a6fe064af6403add11992400
Maxim Muradov <koreantech620@mail.ru>
2024-05-28 02:24:58 +0700

    Add managing jenkins

--- src/test/java/properties/tests/PropertiesSystem.java
+++ src/test/java/properties/tests/PropertiesSystem.java
@@ -12,7 +12,7 @@ public class PropertiesSystem
     @Tag("PracticeForm")
     void BrowserPropertie()
     {
-
+//
         Configuration.remote = System.getProperty("Wdhost","https://user1:1234@selenoid.autotests.cloud/wd/hub");
         browser = System.getProperty("Browser","chrome");
         Configuration.browserVersion = System.getProperty("BrowserVersion","120.0");
