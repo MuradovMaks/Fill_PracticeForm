0266dc65199a0c34d33933cd60605cb251f890f8
Maxim Muradov <koreantech620@mail.ru>
2024-05-28 01:42:35 +0700

    Add managing of parametries

--- src/test/java/properties/tests/PropertiesSystem.java
+++ src/test/java/properties/tests/PropertiesSystem.java
@@ -12,8 +12,8 @@ public class PropertiesSystem
     @Tag("PracticeForm")
     void BrowserPropertie()
     {
-        Configuration.remote = System.getProperty("Wdhost","https://user1:1234@selenoid.autotests.cloud/wd/hub");
         browser = System.getProperty("browser","chrome");
+        Configuration.remote = System.getProperty("Wdhost","https://user1:1234@selenoid.autotests.cloud/wd/hub");
         Configuration.browserSize = System.getProperty("browser_size","1920x1080");
         Configuration.browserVersion = System.getProperty("browser_version","120.0");
     }
